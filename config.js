09eb1b608a9e2ae744f03669ad550a03f2ef02e7
epooldev <34984333+epooldev@users.noreply.github.com>
2018-01-01 00:21:50 +0900

    First Commit
    Add site

---
+++ config.js
@@ -0,0 +1,14 @@
+/* CHANGE ME to real domain */
+var api = "http://168.131.223.170:22022";
+
+var poolHost = "168.131.223.170";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.edollar.cash/block/{id}";
+
+var transactionExplorer = "https://explorer.edollar.cash/tx/{id}";
+
+var themeCss = "themes/nighty-mining-dark-theme.css";
